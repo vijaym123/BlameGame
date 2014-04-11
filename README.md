3d01daccdd052a4012c9ccec01487646f5f64a21
Vijay Mahantesh SM <vijaym123@gmail.com>
2014-04-10 23:00:39 -0500

    Changed the code to directory based data collection

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-ScrapeNewsletters
+Scrape Newsletters
 =================
+
+This project is on collecting data from newsletters and analyzing it. <br>
+
+**Mentored By :** <br/>
+[Laurel Harbridge](http://faculty.wcas.northwestern.edu/~lmh735/)<br/>
+Assistant Professor, Department of Political Science<br/>
+Faculty Fellow, Institute for Policy Research<br/>
+Northwestern University<br/>
+
+[David Doherty](http://orion.luc.edu/~ddoherty/)<br/>
+Assistant Professor,<br/>
+Loyola University Chicago<br/>
+------------------------------------------------------------
--- scrape.py
+++ scrape.py
@@ -1,6 +1,6 @@
 import gmail
-import string
-
+import re
+import os
 
 def loginGmail(filename):
 	f = open(filename,"r")
@@ -8,17 +8,27 @@ def loginGmail(filename):
 	gKey = gmail.login(username, password)
 	return gKey
 
-def writeToFile(data,filename):
-	f = open(filename,"w")
-	line = ""
+def filterBody(string):
+	string = re.sub(r'<https?://[^>]+>',"",string)
+	string = re.sub(r'Click here[^\d]+window',"",string)
+	string = re.sub(r'Click here[^\d]+email',"",string)
+	return string
+
+def writeToFile(data):
 	for mail in data:
-		line = line + mail.fr + "," + str(mail.sent_at) + "," + mail.body + "\n"
-	f.write(line)
+		dirname = mail.fr.split('<')[1].split('@')[0]
+		filename = str(mail.sent_at).replace("-","").replace(" ","").replace(":","") + ".txt"
+		dirpath = os.path.dirname(dirname+"/"+filename)
+		if not os.path.exists(dirpath) :
+			os.makedirs(dirpath)
+		f = open( dirpath + "/" + filename ,"w")
+		line = mail.fr + "\n" + str(mail.sent_at) + "\n" + mail.body + "\n"
+		f.write(filterBody(line))
 	return True
 
 if __name__ == "__main__":
 	gKey = loginGmail("accountDetails")
 	sender = "MD06JDIMA@mail.house.gov"
 	mails=gKey.inbox().mail(sender=sender,prefetch=True)
-	writeToFile(mails,"test.csv")
+	writeToFile(mails)
 
